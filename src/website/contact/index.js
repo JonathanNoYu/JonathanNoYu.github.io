e52f0ba13786f41ec88bc3adbb2eb7e2c234a713
JonathanNoYu <jonathanyu10@gmail.com>
2025-07-30 20:07:55 -0400

    Added window functions & contact pre-message

--- src/website/contact/index.js
+++ src/website/contact/index.js
@@ -3,7 +3,10 @@ import { Row } from "react-bootstrap";
 function Contact() {
   return (
     <>
-      <Row className="d-flex justify-content-center p-auto mt-5">
+      <div className="d-flex justify-content-center">
+        <h2 className="wd-form-pre-message my-5">Contact me via Google Form!</h2>
+      </div>
+      <Row className="d-flex justify-content-center p-auto mt-1">
         <iframe title="Contact-Me"
                 className="wd-dark-filter" 
                 src="https://docs.google.com/forms/d/e/1FAIpQLSeEdkHdyisoJIp9ISxMr8RUX6bsKc3DYxoumNkvUtQ0aiClVA/viewform?embedded=true" 
@@ -11,7 +14,7 @@ function Contact() {
                 height="849" 
                 frameborder="0" 
                 marginheight="0" 
-                marginwidth="0">Loading…</iframe>
+                marginwidth="0"><h1>Loading…</h1></iframe>
       </Row>
     </>
   );
--- src/website/home/index.js
+++ src/website/home/index.js
@@ -2,6 +2,7 @@ import { Row } from "react-bootstrap";
 import NavList from "../NavList";
 import { isVisFunc } from "../../scripts/trackCardVisibilityList";
 import TrackVisibility from "react-on-screen";
+import useWindowDimensions from "../../scripts/windowFunctions";
 
 function Home() {
   const home_comp_names = ["Intro", "Resume"]
@@ -13,12 +14,13 @@ function Home() {
           <h1>
             WIP Intro Check out my gallery instead!!
           </h1>
-          <p>Hi! I'm Jonathan, a new graduate from Northeastern University. 
-            I have a degree in Computer Science and two minors, Music and Business Analytics</p>
+          <p>Hi! I'm Jonathan, a graduate from Northeastern University. 
+            I have a degree in Computer Science and two minors, Music and Business Analytics. I've worked in Java, Javascript, Python, HTML and CSS</p>
         </div>
       </div>
     );
   }
+  
   const TrackedResumeComp = ({ isVisible}) => {
     isVisFunc(isVisible, "Resume")
     return (
@@ -42,12 +44,6 @@ function Home() {
       <TrackVisibility partialVisibility>
            <TrackedResumeComp />
       </TrackVisibility>
-      <div class="input-group date" data-provide="datepicker">
-            <input type="text" class="form-control" />
-            <div class="input-group-addon">
-                <span class="glyphicon glyphicon-th"></span>
-            </div>
-        </div>
     </>
   );
 }
